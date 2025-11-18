ed241cf1f9332fb371985931a7271a7c927910f5
axcvwy <tue131098@protonmail.com>
2025-11-18 09:35:21 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="app-shell">
-    <NavBar />
     <main class="main site container">
       <router-view />
     </main>
-    <FooterBar />
   </div>
 </template>
 
-<script setup lang="ts">
-import NavBar from './components/NavBar.vue'
-import FooterBar from './components/FooterBar.vue'
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
-.container{max-width:var(--max-width);margin:0 auto}
-.main{padding-top:1rem}
+.container {
+  max-width: var(--max-width);
+  margin: 0 auto;
+}
+.main {
+  padding-top: 1rem;
+}
 </style>
